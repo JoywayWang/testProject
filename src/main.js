8528e39a91fee14bd7f83dcf564da7a24fc70bb7
joywaywang <joywaywang@163.com>
2022-07-15 20:59:28 +0800

    临时提交

--- src/main.js
+++ src/main.js
@@ -15,26 +15,26 @@ const apps = [
     entry: 'http://localhost:10086/',
     container: '#test',
     activeRule: '/test'
-  },
-  {
-    name: 'vueApp', // 应用的名字
-    entry: 'http://localhost:10000/', // 默认加载这个html，解析里面的js动态的执行（子应用必须支持跨域，内部使用的是 fetch）
-    container: '#vue', // 要渲染到的容器名id
-    activeRule: '/vue' // 通过哪一个路由来激活
-  },
-  {
-    name: 'reactApp',
-    entry: 'http://localhost:20000/',
-    container: '#react',
-    activeRule: '/react'
-  },
-  {
-    name: 'jqApp',
-    entry: 'http://localhost:5501/qiankun/qiankun-jq/',
-    container: '#jq',
-    activeRule: '/jq',
-    props: {a: 1} // 父应用向子应用传递参数
   }
+  // {
+  //   name: 'vueApp', // 应用的名字
+  //   entry: 'http://localhost:10000/', // 默认加载这个html，解析里面的js动态的执行（子应用必须支持跨域，内部使用的是 fetch）
+  //   container: '#vue', // 要渲染到的容器名id
+  //   activeRule: '/vue' // 通过哪一个路由来激活
+  // },
+  // {
+  //   name: 'reactApp',
+  //   entry: 'http://localhost:20000/',
+  //   container: '#react',
+  //   activeRule: '/react'
+  // },
+  // {
+  //   name: 'jqApp',
+  //   entry: 'http://localhost:5501/qiankun/qiankun-jq/',
+  //   container: '#jq',
+  //   activeRule: '/jq',
+  //   props: {a: 1} // 父应用向子应用传递参数
+  // }
 ];
 
 registerMicroApps(apps); // 注册应用
